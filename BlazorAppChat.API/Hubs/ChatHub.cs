6aa0fbaac1ea63a7c35f1d22d452dab4da0f38a1
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-09-25 01:13:36 -0300

    Initial commit

---
+++ BlazorAppChat.API/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorAppChat.API.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task Broadcast(string username, string message)
+    {
+        await Clients.All.SendAsync("Broadcast", username, message);
+    }
+}
